847d582ca7db2695a96e36e5502be4fc29c27d5d
John <john.whiles@hotmail.com>
2020-04-13 15:07:25 +0100

    Initial Commit Of Tests

---
+++ TDD_Example/Vector3.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TDD_Example
+{
+    public class Vector3
+    {
+        public float x;
+        public float y;
+        public float z;
+
+        public Vector3()
+        {
+            x = 0;
+            y = 0;
+            z = 0;
+        }
+
+        public Vector3(float x, float y, float z)
+        {
+            this.x = x;
+            this.y = y;
+            this.z = z;
+        }
+
+        public float GetMagnitude()
+        {
+            float mag = 0;
+
+            float xSquared = (float)Math.Pow(x, 2);
+            float ySquared = (float)Math.Pow(y, 2);
+            float zSquared = (float)Math.Pow(z, 2);
+
+            mag = (float)Math.Sqrt(xSquared + ySquared + zSquared);
+            return mag;
+        }
+
+        public void Normalize()
+        {
+            Vector3 normal = GetNormal();
+            this.x = normal.x;
+            this.y = normal.y;
+            this.z = normal.z;
+        }
+
+        public Vector3 GetNormal()
+        {
+            float magnitude = GetMagnitude();
+            float x = this.x / magnitude;
+            float y = this.y / magnitude;
+            float z = this.z / magnitude;
+
+            Vector3 result = new Vector3(x, y, z);
+            return result;
+        }
+
+        public float GetDotProduct(Vector3 v2)
+        {
+            float mag1 = this.GetMagnitude();
+            float mag2 = v2.GetMagnitude();
+            float dot = (this.x * v2.x) + (this.y * v2.y) + (this.z * v2.z);
+            float product = dot / (mag1 * mag2);
+            return product;
+        }
+
+        public static Vector3 operator +(Vector3 v1, Vector3 v2)
+        {
+            var result = new Vector3();
+
+            result.x = v1.x + v2.x;
+            result.y = v1.y + v2.y;
+            result.z = v1.z + v2.z;
+
+            return result;
+        }
+    }
+}
---
+++ TDD_ExampleTests/Vector3Tests.cs
@@ -0,0 +1,105 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TDD_Example;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit.Sdk;
+
+namespace TDD_Example.Tests
+{
+    [TestClass()]
+    public class Vector3Tests
+    {
+        [TestMethod()]
+        public void ConstructorDoesNotThrowExceptionTest()
+        {
+            bool threwException = false;
+            try
+            {
+                Vector3 vector = new Vector3();
+            }
+            catch
+            {
+                threwException = true;
+            }
+
+            Assert.IsFalse(threwException);
+        }
+
+        [TestMethod()]
+        public void ConstructorWithParametersWillSetValuesCorrectlyTest()
+        {
+            Vector3 vector = new Vector3(1, 2, 3);
+
+            Assert.AreEqual(1, vector.x);
+            Assert.AreEqual(2, vector.y);
+            Assert.AreEqual(3, vector.z);
+        }
+
+        [TestMethod()]
+        public void AddOverloadIncreasesValueOfVectorByCorrectAmountTest()
+        {
+            Vector3 vector1 = new Vector3(1, 1, 1);
+            Vector3 vector2 = new Vector3(1, 1, 1);
+
+            vector1 = vector1 + vector2;
+
+            Assert.AreEqual(2, vector1.x);
+            Assert.AreEqual(2, vector1.y);
+            Assert.AreEqual(2, vector1.z);
+        }
+
+        [TestMethod()]
+        public void GetMagnitudeReturnsTheCorrectValueTest()
+        {
+            Vector3 vector = new Vector3(5, 5, 5);
+            float mag = vector.GetMagnitude();
+
+            Assert.AreEqual(8.6602545f, mag);
+        }
+
+        [TestMethod()]
+        public void NormalizeVectorWillNormalizeAVectorTest()
+        {
+            Vector3 vector = new Vector3(1, 2, 3);
+            vector.Normalize();
+
+            Assert.AreEqual(0.26726f, vector.x, 0.0001f);
+            Assert.AreEqual(0.5345f, vector.y, 0.0001f);
+            Assert.AreEqual(0.8018f, vector.z, 0.0001f);    
+        }
+
+        [TestMethod()]
+        public void GetNormalVectorWillReturnTheNormalButNotNormalizeTheVectorTest()
+        {
+            Vector3 vector = new Vector3(1, 1, 1);
+            Vector3 normalizedVector = vector.GetNormal();
+
+            Assert.AreEqual(0.5774f, normalizedVector.x, 0.0001f);
+            Assert.AreEqual(0.5774f, normalizedVector.y, 0.0001f);
+            Assert.AreEqual(0.5774f, normalizedVector.z, 0.0001f);
+
+            Assert.AreEqual(1, vector.x);
+            Assert.AreEqual(1, vector.y);
+            Assert.AreEqual(1, vector.z);
+        }
+
+        [TestMethod()]
+        public void GetDotProductReturnsCorrectValueTest()
+        {
+            Vector3 vector1 = new Vector3(1, 2, 3);
+            Vector3 vector2 = new Vector3(4, 5, 6);
+
+            float dot = vector1.GetDotProduct(vector2);
+
+            Assert.AreEqual(0.9746318f, dot, 0.00001f);
+        }
+    }
+}
+
+namespace TDD_ExampleTests
+{
+    class Vector3Tests
+    {
+    }
+}
